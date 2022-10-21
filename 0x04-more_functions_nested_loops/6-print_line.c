8c97872f27dee1348c51d3fb692e5b00ff247bc8
Temba Saina <tembasaina@gmail.com>
2022-10-21 12:49:18 +0200

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_line - print line of argument length
+* @n: integer for length of line
+*/
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
